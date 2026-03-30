7ff70f68d24d8032e87276c9778b3b938857f84f
ain3456 <ain345@naver.com>
2026-03-30 17:09:47 +0900

    Add files via upload

---
+++ more실습_성아인/js/more.js
@@ -0,0 +1,39 @@
+﻿$(function(){
+    let count = 0;
+    let step;
+
+    const items = $("#box ul li");
+    const total = items.length;
+
+    items.hide();
+    function setstep(){
+        const wid = $(window).width();
+        if(wid < 800) step = 2;
+        else if(wid < 1000) step = 3;
+        else step =4;
+    }
+
+    function init(){
+        setstep();
+        count = step;
+
+        items.hide();
+        items.slice(0,count).show();
+    }
+
+    $("#btn").on("click", function(e){
+        e.preventDefault();
+        count = Math.min(count+step,total);
+        items.slice(0,count).fadeIn();
+
+        if(count >= total){
+            $("#btn").fadeOut();
+        }
+    });
+
+    $(window).on("resize",function(){
+        init();
+        $("#btn").show();
+    });
+    init();
+});
