8ce50a7487f6299de4a193e15506d3e1a885f44a
ain3456 <ain345@naver.com>
2026-05-26 13:03:43 +0900

    Add files via upload

---
+++ 애플리케이션배포평가파일_성아인/src/components/TodoList.js
@@ -0,0 +1,19 @@
+import TodoItem from './TodoItem';
+
+function TodoList({
+    todos,
+    deleteTodo,
+    updateTodo,
+    toggleTodo
+}){
+    return(
+        <div>
+            {
+                todos.map(todo => (
+                    <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} toggleTodo={toggleTodo}/>
+                ))
+            }
+        </div>
+    )
+}
+export default TodoList;
